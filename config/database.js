2871cc0332e311b0b93406e303885d6f7f490cfe
Mohamed Amine Mhiri <mohamedamine.mhiri@esprit.tn>
2018-04-27 02:55:44 +0100

    cors prob

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database:'mongodb://localhost/SmarTunis',
+	//database:'mongodb://localhost/SmarTunis',
+	database:'mongodb://smartunis:abcd1234@ds159489.mlab.com:59489/smartunis',
 	secret: ''
 }
